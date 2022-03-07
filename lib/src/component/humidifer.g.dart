5a25f8f20a1bde792253d949ac02aa88d16c4ac4
Carlos F A Gratz <cafe01@gmail.com>
2022-03-07 18:29:29 -0300

    generated humidifier

---
+++ lib/src/component/humidifer.g.dart
@@ -0,0 +1,12 @@
+// This file was auto-generated
+// Do NOT EDIT by hand
+
+import '../component.dart';
+import '../room.dart';
+import 'threshold_relay.g.dart';
+
+class Humidifer extends ThresholdRelay {
+  Humidifer({required Room room}) : super(room: room) {
+    name = "Humidificador";
+  }
+}
