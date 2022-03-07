c16accb900a51d193b93186a0f5e5d628d771f52
Carlos F A Gratz <cafe01@gmail.com>
2022-03-07 18:27:52 -0300

    generated lighting

---
+++ lib/src/component/lighting.g.dart
@@ -0,0 +1,12 @@
+// This file was auto-generated
+// Do NOT EDIT by hand
+
+import '../component.dart';
+import '../room.dart';
+import 'relay.g.dart';
+
+class Lighting extends Relay {
+  Lighting({required Room room}) : super(room: room) {
+    name = "Iluminação";
+  }
+}
