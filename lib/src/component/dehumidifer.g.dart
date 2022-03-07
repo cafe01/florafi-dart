092e1f409fcd3d71512910468f4d6e5f2161c12d
Carlos F A Gratz <cafe01@gmail.com>
2022-03-07 18:30:39 -0300

    generated dehumidifier

--- lib/src/component/dehumidifer.g.dart
+++ lib/src/component/dehumidifer.g.dart
@@ -5,8 +5,8 @@ import '../component.dart';
 import '../room.dart';
 import 'threshold_relay.g.dart';
 
-class Humidifer extends ThresholdRelay {
-  Humidifer({required Room room}) : super(room: room) {
-    name = "Umidificador";
+class Dehumidifer extends ThresholdRelay {
+  Dehumidifer({required Room room}) : super(room: room) {
+    name = "Desumidificador";
   }
 }
