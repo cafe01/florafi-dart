f35105abdb6d149f4e91c4bfc72363e45e01a1d3
Carlos F A Gratz <cafe01@gmail.com>
2022-03-07 18:25:50 -0300

    generated ebbflow_flood

---
+++ lib/src/component/ebbflow_flood.g.dart
@@ -0,0 +1,12 @@
+// This file was auto-generated
+// Do NOT EDIT by hand
+
+import '../component.dart';
+import '../room.dart';
+import 'relay.g.dart';
+
+class EbbflowFlood extends Relay {
+  EbbflowFlood({required Room room}) : super(room: room) {
+    name = "Ebbflow flood";
+  }
+}
