0fe2da085f9e086fc3000fbf278e430f607c529d
Carlos F A Gratz <cafe01@gmail.com>
2022-03-07 20:23:14 -0300

    updated test for Room.resolveComponent()

--- test/room_test.dart
+++ test/room_test.dart
@@ -22,12 +22,93 @@ void main() {
       expect(room.resolveComponent('unkownComponent'), null);
     });
 
+    test('handles Daytime.', () {
+      expect(room.daytime, null);
+      expect(room.resolveComponent('daytime'), isA<Daytime>());
+      expect(room.daytime, isA<Daytime>());
+      expect(room.daytime, same(room.resolveComponent('daytime')));
+    });
+
+    test('handles Dehumidifier.', () {
+      expect(room.dehumidifier, null);
+      expect(room.resolveComponent('dehumidifier-relay'), isA<Dehumidifier>());
+      expect(room.dehumidifier, isA<Dehumidifier>());
+      expect(
+          room.dehumidifier, same(room.resolveComponent('dehumidifier-relay')));
+    });
+
+    test('handles EbbflowDrain.', () {
+      expect(room.ebbflowDrain, null);
+      expect(room.resolveComponent('ebbflow-drain-relay'), isA<EbbflowDrain>());
+      expect(room.ebbflowDrain, isA<EbbflowDrain>());
+      expect(room.ebbflowDrain,
+          same(room.resolveComponent('ebbflow-drain-relay')));
+    });
+
+    test('handles EbbflowFlood.', () {
+      expect(room.ebbflowFlood, null);
+      expect(room.resolveComponent('ebbflow-flood-relay'), isA<EbbflowFlood>());
+      expect(room.ebbflowFlood, isA<EbbflowFlood>());
+      expect(room.ebbflowFlood,
+          same(room.resolveComponent('ebbflow-flood-relay')));
+    });
+
+    test('handles Ebbflow.', () {
+      expect(room.ebbflow, null);
+      expect(room.resolveComponent('ebbflow'), isA<Ebbflow>());
+      expect(room.ebbflow, isA<Ebbflow>());
+      expect(room.ebbflow, same(room.resolveComponent('ebbflow')));
+    });
+
+    test('handles Exaust.', () {
+      expect(room.exaust, null);
+      expect(room.resolveComponent('exaust-relay'), isA<Exaust>());
+      expect(room.exaust, isA<Exaust>());
+      expect(room.exaust, same(room.resolveComponent('exaust-relay')));
+    });
+
+    test('handles Humidifier.', () {
+      expect(room.humidifier, null);
+      expect(room.resolveComponent('humidifier-relay'), isA<Humidifier>());
+      expect(room.humidifier, isA<Humidifier>());
+      expect(room.humidifier, same(room.resolveComponent('humidifier-relay')));
+    });
+
+    test('handles Hygrometer.', () {
+      expect(room.hygrometer, null);
+      expect(room.resolveComponent('humidity'), isA<Hygrometer>());
+      expect(room.hygrometer, isA<Hygrometer>());
+      expect(room.hygrometer, same(room.resolveComponent('humidity')));
+    });
+
+    test('handles IntervalIrrigation.', () {
+      expect(room.intervalIrrigation, null);
+      expect(room.resolveComponent('interval-irrigation'),
+          isA<IntervalIrrigation>());
+      expect(room.intervalIrrigation, isA<IntervalIrrigation>());
+      expect(room.intervalIrrigation,
+          same(room.resolveComponent('interval-irrigation')));
+    });
+
+    test('handles LightSensor.', () {
+      expect(room.lightSensor, null);
+      expect(room.resolveComponent('light'), isA<LightSensor>());
+      expect(room.lightSensor, isA<LightSensor>());
+      expect(room.lightSensor, same(room.resolveComponent('light')));
+    });
+
+    test('handles Lighting.', () {
+      expect(room.lighting, null);
+      expect(room.resolveComponent('light-relay'), isA<Lighting>());
+      expect(room.lighting, isA<Lighting>());
+      expect(room.lighting, same(room.resolveComponent('light-relay')));
+    });
+
     test('handles Thermometer.', () {
       expect(room.thermometer, null);
-      expect(room.resolveComponent('thermometer'), isA<Thermometer>());
       expect(room.resolveComponent('temperature'), isA<Thermometer>());
       expect(room.thermometer, isA<Thermometer>());
-      expect(room.thermometer, same(room.resolveComponent('thermometer')));
+      expect(room.thermometer, same(room.resolveComponent('temperature')));
     });
   });
 }
