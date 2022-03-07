525e7b1c4760f981a75d299054f31c23db9576fe
Carlos F A Gratz <cafe01@gmail.com>
2022-03-07 18:18:23 -0300

    generated daytime component

---
+++ lib/src/component/daytime.g.dart
@@ -0,0 +1,19 @@
+// This file was auto-generated
+// Do NOT EDIT by hand
+
+import '../component.dart';
+import '../room.dart';
+
+class Daytime extends Component {
+  Daytime({required Room room}) : super(room: room) {
+    name = "Fotoperíodo";
+  }
+
+  bool? get isDaytime => getBool("is_daytime");
+  int? get startHour => getInt("start_hour");
+  int? get duration => getInt("duration");
+  int? get startDelay => getInt("start_delay");
+  set startHour(int? value) => setControl("start_hour", value);
+  set duration(int? value) => setControl("duration", value);
+  set startDelay(int? value) => setControl("start_delay", value);
+}
