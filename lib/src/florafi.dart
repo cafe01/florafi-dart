02a715159a5ee36592ef1f09cb268ab118a4ed72
Carlos F A Gratz <cafe01@gmail.com>
2022-03-10 21:15:14 -0300

    implemented Florafi.joinFarm()

--- lib/src/florafi.dart
+++ lib/src/florafi.dart
@@ -1,10 +1,18 @@
-
+import 'package:florafi/src/mqtt_communicator.dart';
+import 'cloud.dart';
 import 'farm.dart';
 
 class Florafi {
+  Farm joinFarm(FarmTicket ticket) {
+    final communicator = MqttCommunicator(
+        server: ticket.host,
+        port: ticket.tlsPort,
+        username: ticket.username,
+        password: ticket.password);
 
-  Farm connect() {
-    throw UnimplementedError();
+    final farm = Farm(
+        name: ticket.farmName, id: ticket.farmId, communicator: communicator);
+
+    return farm;
   }
-
 }
