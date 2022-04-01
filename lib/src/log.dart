4d7e6d002838d6b97ddc675970068c8921e38df0
Carlos F A Gratz <cafe01@gmail.com>
2022-04-01 15:57:54 -0300

    improved LogLine

--- lib/src/log.dart
+++ lib/src/log.dart
@@ -1,8 +1,10 @@
+import 'room.dart';
+
 enum LogLevel { debug, info, warning, error }
 
 class LogLine {
   LogLine(
-      {required this.roomId,
+      {required this.room,
       required this.level,
       required this.message,
       required this.time,
@@ -10,16 +12,19 @@ class LogLine {
       this.componentId});
 
   factory LogLine.fromJson(
-      String roomId, LogLevel level, Map<String, dynamic> json) {
+      Room room, LogLevel level, Map<String, dynamic> json) {
     final time = DateTime.fromMillisecondsSinceEpoch(
         (json['time'] as int) * 1000,
         isUtc: true);
     final message = json['message'] as String;
     final deviceId = json['device'] as String;
     final componentId = json['component'] as String?;
+    if (componentId != null && room.hasComponent(componentId) is! bool) {
+      throw UnknownComponentError(componentId);
+    }
 
     return LogLine(
-        roomId: roomId,
+        room: room,
         level: level,
         time: time,
         message: message,
@@ -27,7 +32,7 @@ class LogLine {
         componentId: componentId);
   }
 
-  final String roomId;
+  final Room room;
   final LogLevel level;
   final DateTime time;
   final String message;
