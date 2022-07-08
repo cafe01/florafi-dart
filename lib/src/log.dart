6ae207cb689f47c27afdd0c81b4b232201d26217
Carlos F A Gratz <cafe01@gmail.com>
2022-07-08 19:18:29 -0300

    LogLine.time now defaults to local time

--- lib/src/log.dart
+++ lib/src/log.dart
@@ -14,8 +14,9 @@ class LogLine {
   factory LogLine.fromJson(
       Room room, LogLevel level, Map<String, dynamic> json) {
     final time = DateTime.fromMillisecondsSinceEpoch(
-        (json['time'] as int) * 1000,
-        isUtc: true);
+            (json['time'] as int) * 1000,
+            isUtc: true)
+        .toLocal();
     final message = json['message'] as String;
     final deviceId = json['device'] as String;
     final componentId = json['component'] as String?;
